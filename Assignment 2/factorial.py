fdfcb39ce1974ec044dd46b622ecc55eb1c394ea
Ankit <57517433+dulatankit@users.noreply.github.com>
2019-12-02 23:17:26 +0530

    Add files via upload

---
+++ Assignment 2/factorial.py
@@ -0,0 +1,27 @@
+
+import timeit
+def factorial(n):  
+   if n == 1:  
+       return n  
+   else:  
+       return n*factorial(n-1)  
+# take input from the user  
+num = int(input("Enter a number: "))  
+start = timeit.default_timer()
+# check is the number is negative  
+if num < 0:  
+   print("Sorry, factorial does not exist for negative numbers")  
+elif num == 0:  
+   print("The factorial of 0 is 1")  
+else:  
+   print("The factorial of",num,"is",factorial(num))  
+stop = timeit.default_timer()
+print('Time in second :', stop - start)  
+
+
+
+
+
+
+
+
